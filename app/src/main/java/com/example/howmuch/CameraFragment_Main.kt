976800767e451f4f5e1332485ff1253edc70aea2
SEEUNby <seeun001229@gmail.com>
2023-01-17 01:38:16 +0900

    오류가 있는 1차 업데이트
    오류가 좀 있습니다,,

---
+++ app/src/main/java/com/example/howmuch/CameraFragment_Main.kt
@@ -0,0 +1,16 @@
+package com.TAVE10thProject.HowMuch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.TAVE10thProject.HowMuch.databinding.FragmentMainCameraBinding
+import com.example.howmuch.databinding.FragmentMainCameraBinding
+
+class CameraFragment_Main :Fragment(){
+    lateinit var binding: FragmentMainCameraBinding
+    //private lateinit var
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentMainCameraBinding.inflate(layoutInflater)
+    }
+}
---
+++ app/src/main/java/com/example/howmuch/MyPageFragment_Main.kt
@@ -0,0 +1,16 @@
+package com.example.howmuch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.TAVE10thProject.HowMuch.databinding.FragmentMainMypageBinding
+import com.example.howmuch.databinding.FragmentMainMypageBinding
+
+class MyPageFragment_Main :Fragment(){
+    lateinit var binding: FragmentMainMypageBinding
+    //private lateinit var
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentMainMypageBinding.inflate(layoutInflater)
+    }
+}
