0fbe27f9ffce2c59bfa6050fb97895790714e62a
SEEUNby <seeun001229@gmail.com>
2023-01-17 17:01:54 +0900

    하단바 클릭하면 보이는 거 완료
    하단바 클릭하면 보입니다 이제

--- app/src/main/java/com/example/howmuch/HomeFragment_Main.kt
+++ app/src/main/java/com/example/howmuch/HomeFragment_Main.kt
@@ -1,8 +1,11 @@
-package com.TAVE10thProject.HowMuch
+package com.example.howmuch
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.TAVE10thProject.HowMuch.databinding.FragmentMainHomeBinding
+import com.example.howmuch.databinding.FragmentMainHomeBinding
 
 class HomeFragment_Main: Fragment() {
     lateinit var binding: FragmentMainHomeBinding
@@ -11,5 +14,18 @@ class HomeFragment_Main: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentMainHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainHomeBinding.inflate(inflater,container,false)
+
+        //token = arguments?.getString("token").toString()
+        //userPK = arguments?.getString("userPK").toString()
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/howmuch/MyPageFragment_Main.kt
+++ app/src/main/java/com/example/howmuch/MyPageFragment_Main.kt
@@ -1,15 +1,30 @@
 package com.example.howmuch
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.howmuch.databinding.FragmentMainMypageBinding
 
 class MyPageFragment_Main :Fragment(){
     lateinit var binding: FragmentMainMypageBinding
-    //private lateinit var
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentMainMypageBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainMypageBinding.inflate(inflater,container,false)
+
+        //token = arguments?.getString("token").toString()
+        //userPK = arguments?.getString("userPK").toString()
+
+        return binding.root
     }
 }
